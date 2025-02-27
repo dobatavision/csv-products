a0bff42a013eaa1a74aced309e7792af797f284e
Dobromir Petrevski <dobromir@enetpulse.com>
2025-02-27 23:32:55 +0200

    add sleep entrypoint

--- .docker/entrypoint_ubuntu.sh
+++ .docker/entrypoint_ubuntu.sh
@@ -8,7 +8,10 @@ fi
 
 service php8.3-fpm start
 service nginx start
+sleep 2
 php composer.phar install
+
+sleep 2
 
 chmod -R 777 /mnt/csv-products/storage
 chmod -R 777 /mnt/csv-products/bootstrap/cache
@@ -18,7 +21,7 @@ php artisan cache:clear
 php artisan view:clear
 php artisan route:clear
 
-service sendmail start -d
+# service sendmail start -d
 
 tail -f /dev/null
 
